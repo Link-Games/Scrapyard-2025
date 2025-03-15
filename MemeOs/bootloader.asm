72c220a920a591c73810f790182e6de6e04dce6d
JxTech <xavierwendt@gmail.com>
2025-03-15 17:03:37 -0400

    im boutta bust

--- MemeOs/bootloader.asm
+++ MemeOs/bootloader.asm
@@ -2,31 +2,30 @@ bits 16
 org 0x7c00
 
 mov [ BOOT_DRIVE ], dl ; BIOS stores our boot drive in dl
-mov al, 'k'
-mov ah, 0x0e
+mov ah,0x0
+mov al,0x13
 int 0x10
-call readdisk
 jmp $
 
 readdisk:
     pusha
     mov ah, 0x02
     mov al, 128
-    mov ch, 0
+    mov ch, CYL_TO_READ
     mov dh, 0 
-    mov cl, SECT_TO_READ
-    mov bx, ADD_TO_WRITE
+    mov cl, 0
+    mov bx, 0x1000
     mov ds, bx
     mov dl, [ BOOT_DRIVE ]
     int 0x13
-    add cl, 128
+    mov ax,0x0000
+    mov ds, ax
+    inc ch
     popa
     ret
 
 BOOT_DRIVE: db 0
-SECT_TO_READ: db 2
-ADD_TO_WRITE: dw 0x1000
-TEST_MESSAGE: db 'Hello World', 13, 10, 0
+CYL_TO_READ: db 1
 
 times 510 - ($-$$) db 0
-dw 0xaa55
+dw 0xAA55
