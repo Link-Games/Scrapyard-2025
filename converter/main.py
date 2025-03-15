2818693918d807bd31a98bb23020723895b4d665
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-03-15 12:03:42 -0700

    Update main.py

--- converter/main.py
+++ converter/main.py
@@ -1,21 +1,20 @@
 from PIL import Image
 
 images = ["MOUGGER", "What She Sees VS What I See"]
+i = 0
+while i < len(images):
+    # Open the image
+    image = Image.open("converter/input/" + images[i] + ".png")
 
-# Open the image
-image = Image.open("converter/inout/" + images[0] + ".png")
+    # Convert the image to RGB mode if it’s not already
+    image = image.convert("RGB")
 
-# Convert the image to RGB mode if it’s not already
-image = image.convert("RGB")
+    # Resize the image to 320x200 pixels
+    resized_image = image.resize((320, 200), Image.Resampling.LANCZOS)
 
-# Resize the image to 320x200 pixels
-resized_image = image.resize((320, 200), Image.Resampling.LANCZOS)
+    # Reduce the image to 256 colors (outputs in "P" mode)
+    quantized_image = resized_image.quantize(colors=256, method=Image.Quantize.MEDIANCUT)
 
-# Reduce the image to 256 colors (outputs in "P" mode)
-quantized_image = resized_image.quantize(colors=256, method=Image.Quantize.MEDIANCUT)
-
-# Save the result as PNG
-quantized_image.save("converter/output/" + images[0] + ".bmp", optimize=True)
-
-# Optional: Display the result
-quantized_image.show()
+    # Save the result as PNG
+    quantized_image.save("converter/output/" + images[i] + ".bmp", optimize=True)
+    i+=1
